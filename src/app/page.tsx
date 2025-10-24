b269dc963a71a34363a70bfb4d284ff031229531
Yu Ling Chen <yo0.guitar.it@gmail.com>
2025-10-24 17:15:20 +0800

    更新音樂連結，修正 Spotify、Apple Music、KKBox、YouTube Music、StreetVoice、Line Music 和 WangYiYun 的音樂專輯連結

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,42 +18,42 @@ export default function Home() {
       name: "Spotify",
       imgSrc: "https://services.linkfire.com/logo_spotify_onlight.svg",
       musicLink:
-        "https://open.spotify.com/album/3b0WhPepXHm3K1YT40cN1y?si=Bi-djGF-RKydYv4Btk8X7Q",
+        "https://open.spotify.com/album/3ZSu3LsLWTSdUzjtzxeXu0?si=LqnGgOsjRS-lfwbQDRb2Fw",
     },
     {
       name: "Apple Music",
       imgSrc: "https://services.linkfire.com/logo_applemusic_onlight.svg",
       musicLink:
-        "https://music.apple.com/tw/album/%E6%97%A5%E5%85%A8%E8%9D%95-ep/1847322570",
+        "https://music.apple.com/tw/album/%E7%8D%B5%E5%BF%83-single/1787969302",
     },
     {
       name: "KKBox",
       imgSrc: "https://www.kkbox.com/about/img/logo/logo.svg",
-      musicLink: "https://kkbox.fm/KaJzRh",
+      musicLink: "https://kkbox.fm/maI785",
     },
     {
       name: "YouTube Music",
       imgSrc:
         "https://upload.wikimedia.org/wikipedia/commons/0/04/YouTube_Music_logo.svg",
       musicLink:
-        "https://music.youtube.com/playlist?list=OLAK5uy_lONO9pGj2MsU-8W8sBvZZ_-aCY8vOAk40&si=VBjAjH9-q4ErGXuD",
+        "https://music.youtube.com/playlist?list=OLAK5uy_mZMBx4smj0YYbYp67OWfPvw5KzcPbNQ-E&si=DERDJTvVbOM-n4U2",
     },
     {
       name: "StreetVoice",
       imgSrc:
         "https://streetvoice.com/pages/mobile-app/static/images/logo-sv-b-s.svg",
       musicLink:
-        "https://streetvoice.com/whitefrequency2016/songs/album/97324127/",
+        "https://streetvoice.com/whitefrequency2016/songs/album/97322807/",
     },
     {
       name: "Line Music",
       imgSrc: "https://cdn.worldvectorlogo.com/logos/line-music.svg",
-      musicLink: "https://music-tw.line.me/album/7787184",
+      musicLink: "https://music-tw.line.me/album/7595782",
     },
     {
       name: "WangYiYun",
       imgSrc: "https://services.linkfire.com/logo_netease_onlight.svg",
-      musicLink: "https://music.163.com/#/album?id=287789645",
+      musicLink: "https://music.163.com/#/album?id=257817510",
     },
   ];
 
